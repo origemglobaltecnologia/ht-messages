963fc3a3115b9addca12ff57f5b79adc6b8c9dfa
CristianoOrigemCamejo <camejocristiano@gmail.com>
2025-10-24 10:19:48 -0300

    correcao do import ht_messages

--- src/main/java/com/ht_messages/WebSocketConfig.java
+++ src/main/java/com/ht_messages/WebSocketConfig.java
@@ -1,5 +1,5 @@
 // com.htmessages.WebSocketConfig
-package com.htmessages;
+package com.ht_messages;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
